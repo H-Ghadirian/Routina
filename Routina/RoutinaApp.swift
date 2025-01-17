7bb149111bb698d8af152d0eeda0a0dace7a5c3d
Hamed Ghadirian <h.ghadirian@outlook.com>
2025-01-17 23:12:43 +0100

    tab badge
    show badge on setting tab if user enable app notification but allow notification on system setting is disabled

--- Routina/RoutinaApp.swift
+++ Routina/RoutinaApp.swift
@@ -4,16 +4,16 @@ import UserNotifications
 @main
 struct RoutinaApp: App {
     let persistenceController = PersistenceController.shared
-
-    init() {
-        requestNotificationPermission()
-    }
+    @State private var showSettingsBadge: Bool = false
 
     var body: some Scene {
         WindowGroup {
             TabView {
                 homeView
                 settingsView
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                requestNotificationPermission()
             }
         }
     }
@@ -31,48 +31,17 @@ struct RoutinaApp: App {
             .tabItem {
                 Label("Settings", systemImage: "gear")
             }
+            .badge(showSettingsBadge ? "!" : nil)
     }
 
     private func requestNotificationPermission() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if let error = error {
-                print("Notification permission error: \(error.localizedDescription)")
-                return
-            }
-
-            if !granted {
-                DispatchQueue.main.async {
-                    #if os(iOS)
-                    showSettingsAlert()
-                    #endif
-                }
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                let systemEnabled = settings.authorizationStatus == .authorized
+                let userEnabled = UserDefaults.standard.bool(forKey: "appSettingNotificationsEnabled")
+                showSettingsBadge = userEnabled && !systemEnabled
             }
         }
     }
-
-#if os(iOS)
-    private func showSettingsAlert() {
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-           let rootViewController = windowScene.windows.first?.rootViewController {
-            rootViewController.present(alertController, animated: true)
-        }
-    }
-
-    private var alertController: UIAlertController {
-        let alert = UIAlertController(
-            title: "Enable Notifications",
-            message: "To receive reminders for your routines, please enable notifications in Settings.",
-            preferredStyle: .alert
-        )
-
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "Open Settings", style: .default) { _ in
-            if let url = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(url)
-            }
-        })
-        return alert
-    }
-#endif
 
 }
