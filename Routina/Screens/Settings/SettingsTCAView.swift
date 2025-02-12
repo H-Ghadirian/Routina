4a56e53c2161d4ab0156372e945e8465ac934a11
Hamed Ghadirian <h.ghadirian@outlook.com>
2025-02-12 16:45:40 +0100

    improve settings tab

--- Routina/Screens/Settings/SettingsTCAView.swift
+++ Routina/Screens/Settings/SettingsTCAView.swift
@@ -6,21 +6,45 @@ struct SettingsTCAView: View {
 
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
-            Form {
-                Section {
-                    Toggle("Enable notifications", isOn: viewStore.binding(
-                        get: \.notificationsEnabled,
-                        send: SettingsFeature.Action.toggleNotifications
-                    ))
-                }
-
-                Section {
-                    Button("Open app settings") {
-                        viewStore.send(.openAppSettingsTapped)
+            NavigationView {
+                Form {
+                    Section(header: Text("Notifications")) {
+                        Toggle("Enable notifications", isOn: viewStore.binding(
+                            get: \.notificationsEnabled,
+                            send: SettingsFeature.Action.toggleNotifications
+                        ))
                     }
+                    
+                    Section {
+                        Button("Open app settings") {
+                            viewStore.send(.openAppSettingsTapped)
+                        }
+                    }
+
+                    Section(header: Text("Support")) {
+                        Button(action: {}) {
+                            HStack {
+                                Image(systemName: "envelope")
+                                    .foregroundColor(.blue)
+                                Text("Contact Us")
+                            }
+                        }
+                    }
+                    aboutSectionView
                 }
+                .navigationTitle("Settings")
             }
-            .navigationTitle("Settings")
+        }
+    }
+
+    private var aboutSectionView: some View {
+        Section(header: Text("About")) {
+            HStack {
+                Text("App Version")
+                Spacer()
+                Text("000")
+                    .foregroundColor(.gray)
+            }
         }
     }
 }
