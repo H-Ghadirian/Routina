b078733c75ac609bb8030365d32f5ddc4629c0e2
Hamed Ghadirian <H.qadirian@gmail.com>
2025-03-07 21:47:55 +0100

    Initial Commit

---
+++ CallMom/CallMomApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallMomApp.swift
+//  CallMom
+//
+//  Created by ghadirianh on 07.03.25.
+//
+
+import SwiftUI
+
+@main
+struct CallMomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallMomTests/CallMomTests.swift
@@ -0,0 +1,17 @@
+//
+//  CallMomTests.swift
+//  CallMomTests
+//
+//  Created by ghadirianh on 07.03.25.
+//
+
+import Testing
+@testable import CallMom
+
+struct CallMomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
