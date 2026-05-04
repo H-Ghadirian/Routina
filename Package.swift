bdfa4f5fb6c25481c84fd98391d725ad1aebce5f
Hamed Ghadirian <h.ghadirian@outlook.com>
2026-05-04 02:03:49 +0200

    Extract CloudKit task record parser

--- Package.swift
+++ Package.swift
@@ -282,6 +282,7 @@ let package = Package(
                 "SharedCore/Sync/CloudKitDirectPullMergeSupport.swift",
                 "SharedCore/Sync/CloudKitDirectPullService.swift",
                 "SharedCore/Sync/CloudKitDirectPullSupport.swift",
+                "SharedCore/Sync/CloudKitDirectPullTaskRecordParser.swift",
                 "SharedCore/Sync/CloudKitPushSubscriptionService.swift",
                 "SharedCore/Sync/CloudKitSyncDiagnostics.swift",
                 "SharedCore/Sync/CloudSyncClient.swift",
